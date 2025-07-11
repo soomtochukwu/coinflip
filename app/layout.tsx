6df1867ed62b71d5892c6343feaedda14fe6ad2d
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-07-11 13:37:08 +0100

    +-

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'HeadsUp',
+  description: 'Where Fortune Favors the Bold',
 }
 
 export default function RootLayout({
